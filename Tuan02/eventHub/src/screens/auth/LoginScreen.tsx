14dc106cb08fa3351c7996ed9be9900b71eb6334
JackWhito <namtrung.work@gmail.com>
2025-02-24 01:21:33 +0700

    Update Login UI

--- Tuan02/eventHub/src/screens/auth/LoginScreen.tsx
+++ Tuan02/eventHub/src/screens/auth/LoginScreen.tsx
@@ -1,17 +1,97 @@
-import { View, Text, Button } from 'react-native';
-import React from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Image, Switch } from 'react-native';
+import React,{useState} from 'react';
+import {
+  ButtonComponent,
+  ContainerComponent,
+  InputComponent,
+  RowComponent,
+  SectionComponent,
+  SpaceComponent,
+  TextComponent,
+} from '../../components';
+import { appColors } from '../../constants/appColors';
+import { Lock, Sms } from 'iconsax-react-native';
 
-const LoginScreen = () => {
+
+const LoginScreen = ({navigation}: any) => {
+  const [email, setEmail] = useState('');
+  const [password, setpassword] = useState('');
+  const [isRemember, setIsRemember] = useState(true);
+  const [isDisable,] = useState(true);
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>LoginScreen</Text>
-      <Button title="Login"
-        onPress={ async () =>
-          await AsyncStorage.setItem('accessToken','trung')
-        }
-        />
-    </View>
+    <ContainerComponent isImageBackground isScroll>
+      <SectionComponent
+        styles={{
+          justifyContent: 'center',
+          alignItems:'center',
+          marginTop: 75,
+        }}>
+          <Image
+            source={require('../../assets/img/logo.png')}
+            style={{
+              width:162,
+              height:114,
+              marginBottom:30,
+            }}
+          />
+        </SectionComponent>
+        <SectionComponent>
+          <TextComponent size={24} title text="Sign In"/>
+          <SpaceComponent height={21}/>
+          <InputComponent
+              value={email}
+              placeholder='Email'
+              onChange={val => setEmail(val)}
+              allowClear
+              affix={<Sms size={22} color={appColors.gray}/>}
+          />
+          <InputComponent
+            value={password}
+            placeholder='Password'
+            onChange={val => setpassword(val)}
+            isPassword
+            allowClear
+            affix={<Lock size={22} color={appColors.gray}/>}
+          />
+          <RowComponent justify="space-between">
+            <RowComponent onPress={() => setIsRemember(!isRemember)}>
+              <Switch
+                trackColor={{true: appColors.primary}}
+                thumbColor={appColors.white}
+                value={isRemember}
+                onChange={() => setIsRemember(!isRemember)}
+              />
+              <SpaceComponent width={4}/>
+              <TextComponent text="Remember me"/>
+            </RowComponent>
+            <ButtonComponent
+              text="Forgot Password?"
+              onPress={() => navigation.navigate('Forgot Password')}
+              type="text"
+            />
+          </RowComponent>
+        </SectionComponent>
+        <SpaceComponent height={16}/>
+        <SectionComponent>
+          <ButtonComponent
+            disable={isDisable}
+            //onPress={handelLogin}
+            text="Sign In"
+            type="primary"
+          />
+        </SectionComponent>
+        {/* <SocialLogin/> */}
+        <SectionComponent>
+          <RowComponent justify="center">
+            <TextComponent text="Dont have an account?"/>
+            <ButtonComponent
+              type="link"
+              text="Sign Up"
+              onPress={() => navigation.navigate('SignUpScreen')}
+            />
+          </RowComponent>
+        </SectionComponent>
+    </ContainerComponent>
   );
 };
 
